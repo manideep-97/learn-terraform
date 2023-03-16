6f17ab04936550ae7e4284fe54fd970cf3495728
manideep <manideepdhamera@gmail.com>
2023-03-16 21:54:19 +0000

    create servers script

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -6,8 +6,8 @@ terraform {
   }
 }
 resource "aws_instance" "ec2" {
-  ami ="ami-0a017d8ceb274537d"
-  instance_type = "t3.micro"
+  ami                  ="ami-0a017d8ceb274537d"
+  instance_type        = "t3.micro"
   vpc_security_group_ids = ["sg-0c2f41ec147dbe0a4"]
   tags = {
     Name = "test"
