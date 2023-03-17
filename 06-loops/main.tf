3f7e02f66286e8fee245f4a5cde1acc47bbee0ce
manideep <manideepdhamera@gmail.com>
2023-03-17 21:53:48 +0000

    create servers script

--- 06-loops/main.tf
+++ 06-loops/main.tf
@@ -3,4 +3,26 @@ variable "d1" {
 }
 resource "null_resource" "nothing" {
 count = var.d1
+
+}
+variable "d2" {
+  default = [apple , banana]
+}
+resource "null_resource" "d2" {
+  count = length(var.d2)
+}
+variable "d3" {
+  default = {
+    apple = {
+      name = "apple"
+       count = 3
+    }
+    banana = {
+      name = "banana"
+      count = 30
+    }
+  }
+}
+resource "null_resource" "d3" {
+  for_each = var.d3
 }
