61b45662c19c0cfbb697d84c7f145775b2931288
manideep <manideepdhamera@gmail.com>
2023-03-17 01:40:45 +0000

    create servers script

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent =  true
-name_regex = "centos-8-Devops-Practice"
+name_regex = "Centos-8-DevOps-Practice"
   owners = ["973714476881"]
 
 }
@@ -10,7 +10,7 @@ name_regex = "centos-8-Devops-Practice"
 
 resource "aws_instance" "frontend" {
   count = 5
-  ami = data.aws_ami.ami.image_id
+  ami =  data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0c2f41ec147dbe0a4"]
 
