5f2e885545ccb9be6230b408bcd780e4f665e481
manideep <manideepdhamera@gmail.com>
2023-03-17 22:17:10 +0000

    create servers script

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,28 +1,22 @@
-//data "aws_ami" "ami" {
- // most_recent =  true
-//name_regex = "Centos-8-DevOps-Practice"
-  //owners = ["973714476881"]
+data "aws_ami" "ami" {
+  most_recent =  true
+name_regex = "Centos-8-DevOps-Practice"
+  owners = ["973714476881"]
 
-//}
+}
+resource "aws_instance" "frontend" {
+  count = length(var.instances)
+ ami =  data.aws_ami.ami.image_id
+instance_type = "t3.micro"
+vpc_security_group_ids = ["sg-0c2f41ec147dbe0a4"]
 
-
-
-
-//resource "aws_instance" "frontend" {
-  //count = length(var.instances)
-// ami =  data.aws_ami.ami.image_id
-//instance_type = "t3.micro"
-//vpc_security_group_ids = ["sg-0c2f41ec147dbe0a4"]
-
-//}
-
-
-//variable "instances" {
-// default = [ "cart", "catalogue", "user", "payment","shipping"]
-//}
-//output "public-ip" {
-//value = aws_instance.frontend.*.public_ip
-//}
+}
+variable "instances" {
+ default = [ "cart", "catalogue", "user", "payment","shipping"]
+}
+output "public-ip" {
+value = aws_instance.frontend.*.public_ip
+}
 
 variable "d1" {
   default = [
